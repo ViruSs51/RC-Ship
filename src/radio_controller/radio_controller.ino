5063a8d200c52d8af05273bffd5ae4be5e5fe0e0
Katsuryoku <vitalieberzoi555@gmail.com>
2025-05-17 04:12:13 +0300

    Completed code for control a ship with radio controller

--- src/radio_controller/radio_controller.ino
+++ src/radio_controller/radio_controller.ino
@@ -1,63 +1,116 @@
 #include <SPI.h>
+#include <nRF24L01.h>
 #include <RF24.h>
+
+// Code of command for ship
+// 00001111 - move to front
+// 11110000 - move to back
+// 10011110 - move to front and rotate to right(with motors)
+// 01011111 - move to front and rotate to left(with motors)
+// 01110001 - move to back and rotate to right(with motors)
+// 11111010 - move to back and rotate to left(with motors)
+
 
 #define CE_PIN 9
 #define CSN_PIN 10
-#define J1Y A0
-#define J1X A1
-#define J2X A2
-
-const byte address[10] = "101010010";
-
-int j1x, j1y;
-int j2x;
+#define J1Y A1
+#define J1X A0
+#define J2Y A2
+#define J2X A3
 
 RF24 radio(CE_PIN, CSN_PIN);
+const byte address[6] = "hghq}";
+
+int j1x, j1y, center_j1x, center_j1y;
+int j2x, j2y, center_j2x, center_j2y;
+
+const char* command = "00001111";
 
 void initRadio() {
   radio.begin();
+  radio.setAddressWidth(5); 
   radio.openWritingPipe(address);
-  radio.setPALevel(RF24_PA_LOW);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.setDataRate(RF24_1MBPS);
+  radio.setChannel(108);
+  radio.setRetries(5, 15); 
   radio.stopListening();
 }
 
-void readJoiticks(int &x1, int &y1, int &x2) {
-  x1 = analogRead(J1X);
-  y1 = analogRead(J1Y);
-  x2 = analogRead(J2X);
+void readJoistick(int &x, int &y, int PIN_X, int PIN_Y) {
+  x = analogRead(PIN_X);
+  y = analogRead(PIN_Y);
+}
+
+void joistickToMotor(int &x, int &y, int &center_x, int &center_y) {
+  x = map(x, center_x, 1023, 0, 255);
+  y = map(y, center_y, 1023, 0, 255);
 }
 
 void setup() {
   Serial.begin(9600);
   pinMode(J1Y, INPUT);
   pinMode(J1X, INPUT);
+  pinMode(J2Y, INPUT);
   pinMode(J2X, INPUT);
 
   initRadio();
+
+  // Centralize joistick
+  readJoistick(center_j1x, center_j1y, J1X, J1Y);
+  readJoistick(center_j2x, center_j2y, J2X, J2Y);
 }
 
 void loop() {
-  readJoiticks(j1x, j1y, j2x);
+  readJoistick(j1x, j1y, J1X, J1Y);
+  readJoistick(j2x, j2y, J2X, J2Y);
+
+  if (j2y >= center_j2y) {
+    j2y = map(j2y, center_j2y, 1023, 0, 255);
+    command = "00001111";
+  } else {
+    j2y = map(j2y, 0, center_j2y, 255, 0);
+    command = "11110000";
+  }
+
+  if (j2x >= center_j2x) {
+    j2x = map(j2x, center_j2x, 1023, 90, 180);
+  } else {
+    j2x = map(j2x, 0, center_j2x, 0, 90);
+  }
+
+  if (j1x >= center_j1x) {
+    if (j1x > center_j1x) {
+      if (command == "00001111") command = "10011110";
+      else command = "01110001";
+    }
+
+    j1x = map(j1x, center_j1x, 1023, 0, 255);
+  } else {
+    if (j1x - 10 < center_j1x) {
+      if (command == "00001111") command = "01011111";
+      else command = "11111010";
+    }
+
+    j1x = map(j1x, 0, center_j1x, 255, 0);
+  }
+
+  char payload[50];
+  snprintf(payload, sizeof(payload), "%s ,%d,%d,%d", command, j1x, j2x, j2y);
+
+  bool result = radio.write(payload, sizeof(payload));
+
+  if (result) {
+    Serial.print("Sended: ");
+  } else {
+    Serial.print("Transmission failed: ");
+  }
+
   Serial.print(j1x);
   Serial.print(" : ");
   Serial.print(j1y);
   Serial.print(" - ");
-  Serial.println(j2x);
-
-  //const char text[] = "Hello, World!";
-  //bool result = radio.write(&text, sizeof(text));
-
-  char payload[32];
-  snprintf(payload, sizeof(payload), "%d,%d,%d", j1x, j1y, j2x);
-
-  bool result = radio.write(payload, strlen(payload) + 1);  // +1 pentru '\0'
-
-  if (result) {
-    Serial.print("Send: ");
-    Serial.println(payload);
-  } else {
-    Serial.println("Transmission failed");
-  }
-
-  delay(500);
+  Serial.print(j2x);
+  Serial.print(" : ");
+  Serial.println(j2y);
 }
--- src/radio_dc_ship/radio_dc_ship.ino
+++ src/radio_dc_ship/radio_dc_ship.ino
@@ -1,45 +1,106 @@
 #include <SPI.h>
+#include <nRF24L01.h>
 #include <RF24.h>
+#include <Servo.h>
 
-#define CE_PIN 4
-#define CSN_PIN 5
-
-const byte address[10] = "101010010";
+#define CE_PIN 9
+#define CSN_PIN 10
+#define IN1 2
+#define IN2 3
+#define IN3 4
+#define IN4 7
+#define ENA 6 
+#define ENB 5
+#define SERVO A0
 
 RF24 radio(CE_PIN, CSN_PIN);
+const byte address[6] = "hghq}";
+
+Servo servo;
+
+int j1x, j1y, j2x, j2y;
+char command[8];
 
 void initRadio() {
   radio.begin();
+  radio.setAddressWidth(5); 
   radio.openReadingPipe(0, address);
-  radio.setPALevel(RF24_PA_LOW);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.setDataRate(RF24_1MBPS);
+  radio.setChannel(108);
+  radio.setRetries(5, 15); 
   radio.startListening();
+}
+
+void initMotors() {
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENA, 0);
+  analogWrite(ENB, 0); 
+}
+
+void control() {
+  if (strcmp(command, "00001111") == 0) {
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    analogWrite(ENA, j2y);
+    analogWrite(ENB, j2y); 
+  } else if (strcmp(command, "11110000") == 0) {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+    analogWrite(ENA, j2y);
+    analogWrite(ENB, j2y); 
+  } else if (strcmp(command, "10011110") == 0 || strcmp(command, "01110001") == 0) {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    analogWrite(ENA, j1x);
+    analogWrite(ENB, j1x); 
+  } else if (strcmp(command, "01011111") == 0 || strcmp(command, "11111010") == 0) {
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+    analogWrite(ENA, j1x);
+    analogWrite(ENB, j1x); 
+  }
+
+  servo.write(j2x);
 }
 
 void setup() {
   Serial.begin(9600);
+  servo.attach(SERVO);
 
   initRadio();
+  initMotors();
 }
 
 void loop() {
   if (radio.available()) {
-    char payload[32] = "";
-    int len = radio.getDynamicPayloadSize();
-    radio.read(&payload, len);
+    char payload[50];
+    radio.read(&payload, sizeof(payload));
 
     Serial.print("Received: ");
     Serial.println(payload);
 
-    int j1x, j1y, j2x;
-    sscanf(payload, "%d,%d,%d", &j1x, &j1y, &j2x);
-
-    Serial.print("J1X: ");
-    Serial.print(j1x);
-    Serial.print(" J1Y: ");
-    Serial.print(j1y);
-    Serial.print(" J2X: ");
-    Serial.println(j2x);
-    Serial.println("---------------");
-    delay(100);
+    sscanf(payload, "%s ,%d,%d,%d", command, &j1x, &j2x, &j2y);
+    command[8] = '\0';
   }
+
+  control();
 }
